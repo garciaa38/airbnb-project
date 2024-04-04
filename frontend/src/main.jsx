b76dd504c762c0ff986e7797fa021f3da293773d
garciaa38 <garciaa38@gmail.com>
2024-04-04 18:57:37 -0400

    set up sessionReducer for logging in through frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -13,13 +14,8 @@ if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
-
-// if (process.env.NODE_ENV !== 'production') {
-//   restoreCSRF();
-//   window.csrfFetch = csrfFetch;
-//   window.store = store;
-// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
